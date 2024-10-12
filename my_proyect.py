8ede3d8133a3909e4a1dec7312a145fe915120d7
MyFirstName MyLastName <my@email.com>
2024-10-12 09:19:54 -0600

    error comlete code

--- my_proyect.py
+++ my_proyect.py
@@ -1,3 +1,6 @@
 import os
 
 print(f"My working directory is {os.getcwd()}")
+
+print "This error message only works in Python 2, but not Python 3 "
+print (    "if I hover over the red line, it will show me a hint")
